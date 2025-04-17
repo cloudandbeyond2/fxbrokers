ab3738d7ced67707f971ec24f8c38d0ce4b905e9
ViEnititydata Technologies <vientitydata@gmail.com>
2025-04-17 10:59:22 +0530

    Updates

--- frontend/app/(common)/review/page.tsx
+++ frontend/app/(common)/review/page.tsx
@@ -1,9 +1,27 @@
-import React from 'react'
+import BestBrokers from "@/components/home-two/BestBrokers";
+import CommitedSuccess from "@/components/home-two/CommitedSuccess";
+import CustomerFeedback from "@/components/home-two/CustomerFeedback";
+import DownloadApp from "@/components/home-two/DownloadApp";
+import Empower from "@/components/home-two/Empower";
+import Footer from "@/components/home-two/Footer";
+import Header from "@/components/home-two/Header";
+import Hero from "@/components/home-two/Hero";
+import HighestRatedBroker from "@/components/home-two/HighestRatedBroker";
+import SearchTool from "@/components/home-two/SearchTool";
+import TopBrokers from "@/components/home-two/TopBrokers";
+import BrokerDetails from "@/components/review/BrokerDetails";
+import RecentUserReview from "@/components/review/RecentUserReview";
+import React from "react";
 
-export default function page() {
+const HomeTwo = () => {
   return (
-    <div>
-      hello
-    </div>
-  )
-}
+    <>
+      <Header />
+      <BrokerDetails />
+      <RecentUserReview/>
+      <Footer />
+    </>
+  );
+};
+
+export default HomeTwo;
